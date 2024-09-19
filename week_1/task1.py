3096741d4a3539f116aa0164b49d4468bcc929e5
Daniil <reactive722@gmail.com>
2024-09-19 18:29:40 +0300

    tasks 1-2 completed

---
+++ week_1/task1.py
@@ -0,0 +1,30 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/letter-combinations-of-a-phone-number/?envType=problem-list-v2&envId=string
+"""
+
+
+def decartProduce(lst1, lst2):
+        if len(lst1) == 0:
+             return lst2
+        if len(lst2) == 0:
+             return lst1
+        return [str(l1) + str(l2) for l1 in lst1 for l2 in lst2]
+
+        
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        digitToLetter = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        combinations = []
+        for digit in digits:
+            combinations = decartProduce(combinations, list(digitToLetter[digit]))
+        return combinations
---
+++ week_1/task2.py
@@ -0,0 +1,22 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/simplify-path/?envType=problem-list-v2&envId=string
+"""
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        symbolsOfPath = [symbol for symbol in path.split('/') if '/' not in symbol and symbol != '']
+        resultPath  = []
+        for symbol in symbolsOfPath:
+            if symbol == '.':
+                continue
+            elif symbol == '..':
+                if len(resultPath) == 0:
+                    continue
+                else:
+                    del resultPath[-1]
+            else:
+                resultPath.append(symbol)
+
+        return "/" + "/".join(resultPath)
