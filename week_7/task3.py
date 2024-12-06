78b9a5475fbcb36f281c47b1a545dd2196fba6dc
danya <reactive722@gmail.com>
2024-12-06 21:11:59 +0300

    week_7 task3 completed; flake8 configuration changed

---
+++ week_7/task3.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problem-list/sliding-window/
+url: hhttps://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters
+"""  # noqa: E501
+
+from collections import defaultdict
+
+
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        if not s:
+            return 0
+        letter_freqs = defaultdict(int)
+        for letter in s:
+            letter_freqs[letter] += 1
+
+        for i, letter in enumerate(s):
+            if letter_freqs[letter] < k:
+                return max(
+                    self.longestSubstring(s[:i], k),
+                    self.longestSubstring(s[i + 1 :], k),
+                )
+        return len(s)
