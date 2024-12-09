6369b7f30a743ce2228e29c384ef2b6c0216be87
danya <reactive722@gmail.com>
2024-12-09 22:52:32 +0300

    week_9 completed

--- week_9/task4.py
+++ week_9/task4.py
@@ -2,3 +2,35 @@
 https://leetcode.com/problem-list/binary-tree/
 url: https://leetcode.com/problems/fruit-into-baskets
 """  # noqa: E501
+
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue = [root]
+        levels = []
+
+        while queue:
+            level = []
+            cur_queue_size = len(queue)
+            for i in range(cur_queue_size):
+                cur_node = queue.pop(0)
+                level.append(cur_node.val)
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+            levels.append(level)
+        return levels[::-1]
--- week_9/task5.py
+++ week_9/task5.py
@@ -2,3 +2,29 @@
 https://leetcode.com/problem-list/binary-tree/
 url: https://leetcode.com/problems/fruit-into-baskets
 """  # noqa: E501
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    previous = None
+
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        self.flatten(root.right)
+        self.flatten(root.left)
+        root.right = self.previous
+        root.left = None
+        self.previous = root
