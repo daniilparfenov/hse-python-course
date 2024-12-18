873ccb947cc6158d54a2000606b6239e61243fef
danya <reactive722@gmail.com>
2024-12-18 23:56:33 +0300

    week_10 completed

--- week_10/task5.py
+++ week_10/task5.py
@@ -2,3 +2,27 @@
 https://leetcode.com/problem-list/binary-tree/
 url: https://leetcode.com/problems/sum-root-to-leaf-numbers
 """  # noqa: E501
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorder(self, node, path):
+        if not node:
+            return
+        self.inorder(node.left, path)
+        path.append(node.val)
+        self.inorder(node.right, path)
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        path = []
+        self.inorder(root, path)
+        return path[k - 1]
