27acbf49b466867c69cb42314b953e1cdc187515
danya <reactive722@gmail.com>
2024-12-18 23:42:05 +0300

    week_10 task4 completed

--- week_10/task4.py
+++ week_10/task4.py
@@ -1,4 +1,43 @@
 """
 https://leetcode.com/problem-list/binary-tree/
-url: https://leetcode.com/problems/sum-root-to-leaf-numbers
+url: https://leetcode.com/problems/populating-next-right-pointers-in-each-node
 """  # noqa: E501
+
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(
+        self,
+        val: int = 0,
+        left: "Node" = None,
+        right: "Node" = None,
+        next: "Node" = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: "Optional[Node]") -> "Optional[Node]":
+        if not root:
+            return root
+        queue = [root]
+
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.pop(0)
+                if i == n - 1:
+                    node.next = None
+                else:
+                    node.next = queue[0]
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
