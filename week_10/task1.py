23b3fc9e11a17c7c28c1187cd15594c2a6522aa6
danya <reactive722@gmail.com>
2024-12-18 20:41:34 +0300

    week_10 task1 completed

---
+++ week_10/task1.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problem-list/binary-tree/
+url: https://leetcode.com/problems/recover-binary-search-tree
+"""  # noqa: E501
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def dfs(self):
+        pass
+
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        pass
